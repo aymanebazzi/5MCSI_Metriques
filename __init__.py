0a2e6dbe5b187a57adb85ef4fabfa44d1d3e1074
aymanebazzi <aymanebazzi.myges@gmail.com>
2024-09-12 16:10:18 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -13,6 +13,10 @@ def MaPremiereAPI():
 def hello_world():
     return render_template('hello.html') #comm2
 
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
+  
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
