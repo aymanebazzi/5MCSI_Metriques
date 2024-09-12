4030b4fc1a219539c4fbf85ece5e4dfe4571f490
aymanebazzi <aymanebazzi.myges@gmail.com>
2024-09-12 17:01:39 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,12 +6,13 @@ from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
-@app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"                                                                                                                                 
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #comm2
+
+@app.route('/contact/')
+def MaPremiereAPI():
+    return render_template("contact.html")
 
 @app.route('/tawarano/')
 def meteo():
@@ -21,14 +22,17 @@ def meteo():
     results = []
     for list_element in json_content.get('list', []):
         dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
-  
 
-@app.route("/rapport/")
+@app.route('/rapport/')
 def mongraphique():
     return render_template("graphique.html")
+
+@app.route('/histogramme/')
+def monhistogramme():
+    return render_template("histogramme.html")
 
 if __name__ == "__main__":
   app.run(debug=True)
